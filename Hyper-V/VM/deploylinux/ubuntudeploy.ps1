23f41499e2022b2f08a56e2b6a920900fbb3a7cd
Adam Kazmierczak <adka@powel.com>
2020-12-21 15:51:49 +0100

    adding content

--- Hyper-V/VM/deploylinux/ubuntudeploy.ps1
+++ Hyper-V/VM/deploylinux/ubuntudeploy.ps1
@@ -10,21 +10,13 @@ $scriptblockcontent =
         
         #vm name definition
         $vmName = 'vhsjkplixansible002' # can be changed
-        $fqdn = $vmName +".powelasa.powel.com"
+        $fqdn = $vmName +".some.domain.com"
         
         #future use public key
         #$rootPublicKey = Get-Content "$env:USERPROFILE\.ssh\id_rsa.pub"
 
         # Variables for master token 
         $fileName ="e:\automation\New-VMFromUbuntuImage.ps1" # File where changes regarding token will be applied
-       # $patern = '--token pzp7dtgsdz5rrdx2vschlrr6kxf4z69xq2k2rf8wxqxj4dk4f7pm96aaaaaaaaaa' #Example old token for master replacement
-       # $replaceme = '--token 4j7nqb2xrcqk2cbs4grhg548jmdzf9vhxhsqng87zsr5s4tgbd7kdv'  # Example new token for master /
-
-        #replacement of new token
-      #  Get-Content -path $fileName |ForEach-Object {$_ -replace $patern , $replaceme } |Set-Content f:\automation\new_version_New-VMFromUbuntuImageadddomain.ps1.ps1
-
-      #  Remove-Item -Path f:\automation\New-VMFromUbuntuImageaddomain.ps1 -Force
-      #  Rename-Item -Path f:\automation\new_version_New-VMFromUbuntuImageadddomain.ps1.ps1 -NewName f:\automation\New-VMFromUbuntuImageaddomain.ps1 -Force
 
         #main deploy script
         e:\automation\New-VMFromUbuntuImageadddomain.ps1 -SourcePath $imgFile -VMName $vmName -FQDN $fqdn -VHDXSizeBytes 60GB -MemoryStartupBytes 4GB -ProcessorCount 2 -installdocker -RootPassword Powel2018 -Verbose -Debug -SwitchName "switch vhsjkp001  Lan"
